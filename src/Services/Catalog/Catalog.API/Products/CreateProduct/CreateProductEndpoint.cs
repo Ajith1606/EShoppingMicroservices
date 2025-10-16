5f414a1a61bc1557790aaa7b817e54e93fd50412
Ajith Kumar <ajith16062001@gmail.com>
2025-10-17 01:23:00 +0530

    Develop Create Product Endpoint with Minimal Apis and Carter

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -1,5 +1,17 @@
-﻿namespace Catalog.API.Products.CreateProduct
+﻿using BuildingBlocks.CQRS;
+
+namespace Catalog.API.Products.CreateProduct
 {
+    public record CreateProductRequest
+     (
+     string Name,
+     List<string> Categories,
+     string Description,
+     string ImageFile,
+     decimal Price,
+     int Stock
+     ) : ICommand<CreateProductResult>;
+    public record CreateProductResponse(Guid Id);
     public class CreateProductEndpoint
     {
     }
