2f6a85b5d6d8db496716286d52a272ae871ff73c
Ajith Kumar <ajith16062001@gmail.com>
2025-10-17 01:50:23 +0530

    Develop POST Endpoint with Carter implements ICarterModule for Minimal Apis - 2

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -1,5 +1,4 @@
-﻿using BuildingBlocks.CQRS;
-
+﻿
 namespace Catalog.API.Products.CreateProduct
 {
     public record CreateProductRequest
@@ -12,7 +11,23 @@ namespace Catalog.API.Products.CreateProduct
      int Stock
      );
     public record CreateProductResponse(Guid Id);
-    public class CreateProductEndpoint
+    public class CreateProductEndpoint : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+           app.MapPost("/products", async (CreateProductRequest request, ISender sender) =>
+            {
+                var command = request.Adapt<CreateProductCommand>();
+                var result = await sender.Send(command);
+                var response = request.Adapt<CreateProductResponse>();
+                return Results.Created($"/products/{response.Id}", response);
+            })
+            .WithName("CreateProduct")
+            .Produces<CreateProductResponse>(StatusCodes.Status201Created)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .ProducesProblem(StatusCodes.Status500InternalServerError)
+            .WithSummary("Create Product")
+            .WithDescription("Create Product");
+        }
     }
 }
