f0cac171fdfbfc1997b5d7ee580d36326cf88e2d
Ajith Kumar <ajith16062001@gmail.com>
2025-10-17 01:10:08 +0530

    Implement CQRS Abstractions ICommand into Vertical Slice Feature Handler class

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,4 +1,6 @@
-﻿using MediatR;
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+using MediatR;
 
 namespace Catalog.API.Products.CreateProduct
 {
@@ -10,15 +12,34 @@ namespace Catalog.API.Products.CreateProduct
         string ImageFile,
         decimal Price,
         int Stock
-        ) : IRequest<CreateProductResult>;
+        ) : ICommand<CreateProductResult>;
     public record CreateProductResult(Guid Id);
 
-    internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+    internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
-        public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
         {
             //Bussiness Logic to create a product
-            throw new NotImplementedException();
+
+            //Create Product entity from command object
+            //Save the product entity to database
+            //Return CreateProductResult result
+
+            //Create Product entity from command object
+            var product = new Product
+            {             
+                Name = command.Name,
+                Categories = command.Categories,
+                Description = command.Description,
+                ImageFile = command.ImageFile,
+                Price = command.Price
+            };
+            //TODO
+            //Save the product entity to database
+            
+            //Return CreateProductResult result
+            return new CreateProductResult(Guid.NewGuid());
+           
         }
     }
 }
