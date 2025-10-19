b91111766e834c1b6d07791b71526e6b9576674b
Ajith Kumar <ajith16062001@gmail.com>
2025-10-19 22:11:44 +0530

    Develop GetProducts Feature in Vertical Slice with CQRS, Mediatr in Handler Class

---
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
@@ -0,0 +1,20 @@
+﻿namespace Catalog.API.Products.GetProducts
+{
+    public record GetProductsQuery() : IQuery<GetProductsResult>;
+    public record GetProductsResult(IEnumerable<Product> Products);
+    internal class GetProductsQueryHandler
+        (IDocumentSession session, ILogger<GetProductsQueryHandler> logger)
+            : IQueryHandler<GetProductsQuery, GetProductsResult>
+    {
+        public async Task<GetProductsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("GetProductsQueryHandler.Handle called with {@Query}", query);
+            //Business Logic to get products
+            //Fetch products from database
+            var products = await session.Query<Product>().ToListAsync(cancellationToken);
+            //Return GetProductsResult result
+            return new GetProductsResult(products);
+        }
+    {
+    }
+}
