7dd2aac1cfbcb71873e1c6f21ec7b94e90e6e18e
Ajith Kumar <ajith16062001@gmail.com>
2025-10-21 23:09:33 +0530

    Develop GetProductById Handler with CQRS and MediatR

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductsByIdHandler.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Catalog.API.Products.GetProductById
+{
+    public record GetProductByIdQuery(Guid Id) : IQuery<GetProductByIdRequest>;
+    public record GetProductByIdRequest(Product Product);
+    internal class GetProductsByIdQueryHandler(IDocumentSession session,
+        ILogger<GetProductsByIdQueryHandler> logger) 
+        : IQueryHandler<GetProductByIdQuery, GetProductByIdRequest>
+    {
+        public async Task<GetProductByIdRequest> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("GetProductsByIdQueryHandler.Handle called with {@Query}", query);
+
+            var product = await session.LoadAsync<Product>(query.Id, cancellationToken);
+
+            if (product == null)
+            {
+                
+                throw new ProductNotFoundException();
+                
+                }
+
+            return new GetProductByIdRequest(product);
+        }
+    }
+}
