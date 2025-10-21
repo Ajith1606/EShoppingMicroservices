4c425be91775053c8cc38fbf7ae56723c1bf0d0b
Ajith Kumar <ajith16062001@gmail.com>
2025-10-21 23:16:06 +0530

    Develop GetProductById GET Endpoint with Minimal Apis and Carter

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdEndpoints.cs
@@ -0,0 +1,24 @@
+﻿
+using Catalog.API.Products.GetProducts;
+
+namespace Catalog.API.Products.GetProductById
+{
+    public record GetProductsByIdResponse(Product Product);
+    public class GetProductByIdEndpoints : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/products{id}", async (Guid id, ISender sender) =>
+            {
+                var result = await sender.Send(new GetProductByIdQuery(id));
+                var response = result.Adapt<GetProductsByIdResponse>();
+                return Results.Ok(response);
+            })
+           .WithName("GetProductById")
+           .Produces<GetProductResponse>(StatusCodes.Status200OK)
+           .ProducesProblem(StatusCodes.Status500InternalServerError)
+           .WithSummary("Get Product By Id")
+           .WithDescription("Get Product By Id");
+        }
+    }
+}
--- src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductsByIdEndpoints.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Catalog.API.Products.GetProductById
-{
-    public class GetProductsByIdEndpoints
-    {
-    }
-}
