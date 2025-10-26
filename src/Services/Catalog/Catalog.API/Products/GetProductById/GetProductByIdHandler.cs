80cdb983590b5681dfc246b8558267fabf006a0e
Ajith Kumar <ajith16062001@gmail.com>
2025-10-26 22:00:24 +0530

    Test Logging Behaviour for all MediatR Requests on Catalog.API microservices

--- src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdHandler.cs
@@ -3,14 +3,11 @@ namespace Catalog.API.Products.GetProductById
 {
     public record GetProductByIdQuery(Guid Id) : IQuery<GetProductByIdRequest>;
     public record GetProductByIdRequest(Product Product);
-    internal class GetProductsByIdQueryHandler(IDocumentSession session,
-        ILogger<GetProductsByIdQueryHandler> logger) 
+    internal class GetProductsByIdQueryHandler(IDocumentSession session) 
         : IQueryHandler<GetProductByIdQuery, GetProductByIdRequest>
     {
         public async Task<GetProductByIdRequest> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
-        {
-            logger.LogInformation("GetProductsByIdQueryHandler.Handle called with {@Query}", query);
-
+        {           
             var product = await session.LoadAsync<Product>(query.Id, cancellationToken);
 
             if (product == null)
--- src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
@@ -21,18 +21,15 @@ namespace Catalog.API.Products.UpdateProduct
         }
     }
 
-    public class UpdateProductCommandHandler
-        (IDocumentSession session, ILogger<UpdateProductCommandHandler> logger)
+    public class UpdateProductCommandHandler(IDocumentSession session)
         : ICommandHandler<UpdateProductCommand, UpdateProductResult>
     {
         public async Task<UpdateProductResult> Handle(UpdateProductCommand command, CancellationToken cancellationToken)
-        {
-            logger.LogInformation("UpdateProductHandler.Handle called with {@Command}", command);
+        {           
             var product = await session.LoadAsync<Product>(command.Id, cancellationToken);
             
             if (product == null)
-            {
-                logger.LogWarning("Product with Id {ProductId} not found", command.Id);
+            {             
                throw new ProductNotFoundException(command.Id);
             }
             product.Name = command.Name;
