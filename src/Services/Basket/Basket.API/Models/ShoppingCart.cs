1322a6832bc9fbb4159f8baf29efeb3faa7a5505
Ajith Kumar <ajith16062001@gmail.com>
2025-11-01 07:53:40 +0530

    Develop Vertical Slice Feature Folder with CQRS and MediatR

--- src/Services/Basket/Basket.API/Models/ShoppingCart.cs
+++ src/Services/Basket/Basket.API/Models/ShoppingCart.cs
@@ -4,16 +4,16 @@
     {
         public string UserName { get; set; } = default!;
         public List<ShoppingCartItem> Items { get; set; } = new();
-        public decimal TotalPrice => Items.Sum(item => item.Price * item.Quantity);
+        public decimal TotalPrice => Items.Sum(x => x.Price * x.Quantity);
 
         public ShoppingCart(string userName)
         {
             UserName = userName;
         }
-        // Required for Mapping
+
+        //Required for Mapping
         public ShoppingCart()
         {
-            
         }
     }
 }
--- src/Services/Basket/Basket.API/Models/ShoppingCartItem.cs
+++ src/Services/Basket/Basket.API/Models/ShoppingCartItem.cs
@@ -7,7 +7,5 @@
         public decimal Price { get; set; } = default!;
         public Guid ProductId { get; set; } = default!;
         public string ProductName { get; set; } = default!;
-
-
     }
 }
