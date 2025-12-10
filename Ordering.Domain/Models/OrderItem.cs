aedc00f0aa438c23b8413880922a7e3aee79cad6
Ajith Kumar <ajith16062001@gmail.com>
2025-12-11 01:13:59 +0530

    Develop ValueObjects(Address,Payment) DDD Rich-domain model Entity

--- Ordering.Domain/Models/OrderItem.cs
+++ Ordering.Domain/Models/OrderItem.cs
@@ -11,7 +11,7 @@ namespace Ordering.Domain.Models
     {
         internal OrderItem(OrderId orderId, ProductId productId, int quantity, decimal price)
         {        
-            OrderId = orderId;
+            Id = OrderItemId.Of(Guid.NewGuid()); 
             ProductId = productId;
             Quantity = quantity;
             Price = price;
