3d76002225bbf197f768d670e7756165408f1f04
Ajith Kumar <ajith16062001@gmail.com>
2025-12-23 19:28:56 +0530

    Auto Migrate EF Core Entities to SQL Server when Application Startup w/Coding

--- src/Services/Ordering/Ordering.Infrastructure/GlobalUsing.cs
+++ src/Services/Ordering/Ordering.Infrastructure/GlobalUsing.cs
@@ -7,3 +7,4 @@ global using System.Reflection;
 global using Microsoft.EntityFrameworkCore.Metadata.Builders;
 global using Ordering.Domain.ValueObjects;
 global using Ordering.Domain.Enums;
+global using Microsoft.AspNetCore.Builder;
