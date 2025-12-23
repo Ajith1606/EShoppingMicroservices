9537a7264c4fd08a2b1e3a5e4f16409d8eaa7dd7
Ajith Kumar <ajith16062001@gmail.com>
2025-12-23 20:13:23 +0530

    Run Order Microservices to Migrate and Seed OrderDb in SQL Server using EF Core

--- src/Services/Ordering/Ordering.Infrastructure/Data/Extensions/InitialData.cs
+++ src/Services/Ordering/Ordering.Infrastructure/Data/Extensions/InitialData.cs
@@ -30,7 +30,7 @@
                 var order1 = Order.Create(
                                 OrderId.Of(Guid.NewGuid()),
                                 CustomerId.Of(new Guid("58c49479-ec65-4de2-86e7-033c546291aa")),
-                                OrderName.Of("Order 1"),
+                                OrderName.Of("ORD_1"),
                                 shippingAddress: address1,
                                 billingAddress: address1,
                                 payment1);
@@ -40,7 +40,7 @@
                 var order2 = Order.Create(
                                 OrderId.Of(Guid.NewGuid()),
                                 CustomerId.Of(new Guid("189dc8dc-990f-48e0-a37b-e6f2b60b9d7d")),
-                                OrderName.Of("Order 2"),
+                                OrderName.Of("ORD_2"),
                                 shippingAddress: address2,
                                 billingAddress: address2,
                                 payment2);
