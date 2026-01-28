caca250155ac296224ad5feb2a2b8de0831a24ea
Ajith Kumar <ajith16062001@gmail.com>
2026-01-28 22:52:18 +0530

    Develop Orders UpdateOrderCommand and Handler using IapplicationDbContext

--- src/Services/Ordering/Ordering.Application/GlobalUsing.cs
+++ src/Services/Ordering/Ordering.Application/GlobalUsing.cs
@@ -2,4 +2,6 @@
 global using Ordering.Application.Dtos;
 global using Ordering.Domain.Models;
 global using Ordering.Domain.ValueObjects;
+global using BuildingBlocks.Exceptions;
+global using BuildingBlocks.CQRS;
 
